55b990f5d839398a1472265540cffb88f17e0312
mfearing <34196737+mfearing@users.noreply.github.com>
2024-09-18 14:06:00 -0500

    Added user preferences to AuthenticationService.  Also changed JWT algorithm from HMAC to SHA256 with public and private keys so that microservices can perform public key validation of tokens.

--- AuthenticationService/src/main/java/com/mjf/recipe/AuthenticationService/config/UserAuthenticationProvider.java
+++ AuthenticationService/src/main/java/com/mjf/recipe/AuthenticationService/config/UserAuthenticationProvider.java
@@ -8,56 +8,59 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 import com.mjf.recipe.AuthenticationService.dtos.UserDTO;
 import com.mjf.recipe.AuthenticationService.services.UserService;
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
-import java.util.Base64;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.util.Collections;
 import java.util.Date;
 
 @RequiredArgsConstructor
 @Component
+@Resource
 public class UserAuthenticationProvider {
 
-    @Value("${security.jwt.token.secret-key:secret-key}")
-    private String secretKey;
+    @Value("classpath:keys/privateKey.pem")
+    private RSAPrivateKey privateKey;
 
+    @Value("classpath:keys/publicKey.pem")
+    private RSAPublicKey publicKey;
+
+    private Algorithm rsa256;
+    private JWTVerifier verifier;
     private final UserService userService;
 
     @PostConstruct
-    protected void init(){
-        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    protected void init() {
+        this.rsa256 = Algorithm.RSA256(publicKey, privateKey);
+        this.verifier = JWT.require(this.rsa256).build();
     }
 
     public String createToken(UserDTO user){
         Date now = new Date();
-        Date validity = new Date(now.getTime() + 3600000); // 1 hour
+        Date validity = new Date(now.getTime() + 360000); // 10 min
 
         //TODO consider changing to RSA256 asymmetric encryption for public key verification
-        Algorithm algorithm = Algorithm.HMAC256(secretKey);
         return JWT.create()
                 .withSubject(user.getLogin())
                 .withIssuedAt(now)
                 .withExpiresAt(validity)
                 .withClaim("role", user.getRole().name())
-                .sign(algorithm);
+                .sign(rsa256);
     }
 
     public DecodedJWT validateToken(String token) throws JWTVerificationException {
-        Algorithm algorithm = Algorithm.HMAC256(secretKey);
-        JWTVerifier verifier = JWT.require(algorithm).build();
         return verifier.verify(token.replace("Bearer ", ""));
     }
 
     public Authentication validateTokenAndSetAuthentication(String token){
-        Algorithm algorithm = Algorithm.HMAC256(secretKey);
-        JWTVerifier verifier = JWT.require(algorithm).build();
         DecodedJWT decodedJWT = verifier.verify(token);
         UserDTO user = userService.findByLogin(decodedJWT.getSubject());
-
         return new UsernamePasswordAuthenticationToken(user, null, Collections.singletonList(user.getRole()));
     }
 
--- AuthenticationService/src/main/java/com/mjf/recipe/AuthenticationService/mappers/UserMapper.java
+++ AuthenticationService/src/main/java/com/mjf/recipe/AuthenticationService/mappers/UserMapper.java
@@ -1,10 +1,15 @@
 package com.mjf.recipe.AuthenticationService.mappers;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mjf.recipe.AuthenticationService.dtos.SignUpDTO;
 import com.mjf.recipe.AuthenticationService.dtos.UserDTO;
 import com.mjf.recipe.AuthenticationService.entities.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Uses mapstruct to map a User to a UserDTO, or a SignUpDTO to a User
@@ -12,10 +17,47 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    UserDTO toUserDto(User user);
+    @Mapping(target = "preferences", source = "preferences")
+    @Mapping(target = "token", ignore = true)
+    UserDTO toUserDTO(User user);
+
+    @Mapping(target = "preferences", source = "preferences")
+    @Mapping(target = "password", ignore = true)
+    User toUser(UserDTO userDTO);
 
     //ignore the password because it needs to be encoded
     @Mapping(target = "password", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "role", ignore = true)
+    @Mapping(target = "preferences", ignore = true)
     User signUpToUser(SignUpDTO signUpDTO);
+
+    // Custom method to map String (JSON) to Map<String, Object>
+    default Map<String, Object> mapStringToMap(String preferencesJson) {
+        if (preferencesJson == null || preferencesJson.isEmpty()) {
+            return new HashMap<>();
+        }
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.readValue(preferencesJson, new TypeReference<Map<String, Object>>() {});
+        } catch (Exception e) {
+            throw new RuntimeException("Error parsing JSON", e);
+        }
+    }
+
+    // Custom method to map Map<String, Object> to String (JSON)
+    default String mapMapToString(Map<String, Object> preferences) {
+        if (preferences == null || preferences.isEmpty()) {
+            return "";
+        }
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.writeValueAsString(preferences);
+        } catch (Exception e) {
+            throw new RuntimeException("Error converting Map to JSON", e);
+        }
+    }
 
 }
