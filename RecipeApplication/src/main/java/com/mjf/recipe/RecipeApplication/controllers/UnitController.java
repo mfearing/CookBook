592edc097fae715a003b0cfaf0c9e35e558708fc
mfearing <34196737+mfearing@users.noreply.github.com>
2024-09-27 09:38:31 -0500

    Updated auth, recipe, ingredient, and unit endpoints to match api conventions.

--- RecipeApplication/src/main/java/com/mjf/recipe/RecipeApplication/controllers/UnitController.java
+++ RecipeApplication/src/main/java/com/mjf/recipe/RecipeApplication/controllers/UnitController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/unit")
+@RequestMapping("/units")
 public class UnitController {
     private static final Logger logger = LoggerFactory.getLogger(UnitController.class);
 
